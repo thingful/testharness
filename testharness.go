216a5e72fbbfc130395a75656726ce0351fb6786
dotmancando <dotmancando@gmail.com>
2016-09-21 12:08:53 +0100

    first commit from dot's local

---
+++ testharness.go
@@ -0,0 +1,35 @@
+package testharness
+
+import (
+	"fmt"
+	"github.com/thingful/thingfulx"
+)
+
+var Number = "0"
+
+// var Fetcher thingfulx.Fetcher
+
+func Init(s string) {
+
+	var Number = s
+	fmt.Printf("input = %s\n", Number)
+}
+
+func Register(builder thingfulx.FetcherBuilder) (*Harness, error) {
+	fetcher, err := builder()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Harness{
+		fetcher: fetcher,
+	}, nil
+}
+
+type Harness struct {
+	fetcher thingfulx.Fetcher
+}
+
+func (h *Harness) Run() {
+	fmt.Println("Running fetcher:", h.fetcher.Provider().UID)
+}
